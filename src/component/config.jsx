b55a480bd747e710f8721217d00dacf2bf4d76ef
darshak kanani <kdarshak2910@gmail.com>
2024-08-07 12:16:28 +0530

    live url change

--- src/component/config.jsx
+++ src/component/config.jsx
@@ -1,3 +1,3 @@
 export const API_URL = 'http://192.168.29.124:5000/api/v1/'
 
-export const LIVE_URL = 'https://cors-anywhere.herokuapp.com/http://143.244.139.153:5000/api/v1/'
+export const LIVE_URL = 'https://invitationpost.netlify.apphttps://invitationpost.netlify.app/http://143.244.139.153:5000/api/v1/'
